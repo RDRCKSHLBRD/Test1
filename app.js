af74fca0da26d8e3aa6040a3e6ce06410a011bbe
Roderick Shoolbraid <rdxenv@gmail.com>
2024-12-25 09:59:10 -0800

    basic

---
+++ app.js
@@ -0,0 +1,13 @@
+//app.js
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+module.exports = app;
